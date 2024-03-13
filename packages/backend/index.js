09a8dbdfb96d1fb1518e5d3e3f9ea70faa0b157d
Shakira Pingue <sm880@kent.ac.uk>
2024-03-13 03:08:33 +0000

    Add AWS SDK

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -6,8 +6,8 @@ import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import assetsRouter from "./services/assets/index.js";
-import { secret } from '@aws-amplify/backend';
-// import { secret } from 'aws-amplify';
+import aws from 'aws-sdk';
+
 // import gameAssetsRouter from "./services/assets/game-assets/index.js";
 import { 
   catchAllErrorHandler, 
@@ -28,23 +28,23 @@ app.use(cors());
 app.use(express.json());
 
 // CONNECT TO MONGO
-const uri = process.env.MONGO_URI; // Add your connection string from Atlas to your .env file. See https://docs.atlas.mongodb.com/getting-started/
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const uri = process.env.MONGO_URI; // Add your connection string from Atlas to your .env file. See https://docs.atlas.mongodb.com/getting-started/
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-client.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MongoDB", err);
-    return;
-  }
-  console.log("Connected to MongoDB");
-  client.close();
-});
+// client.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MongoDB", err);
+//     return;
+//   }
+//   console.log("Connected to MongoDB");
+//   client.close();
+// });
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -65,11 +65,20 @@ app.use(catchAllErrorHandler);
 // CREATE APP TABLE
 console.table(listEndpoints(app));
 
+const ssm = new aws.SSM({region: 'us-east-1'});
+
+const parameterPromise =  await ssm.getParameter({
+  Name: '/amplify/dxrmrwkfi1p5r/dynamo-learning-app-backend/MONGO_URI',
+  WithDecryption: true
+})
+
+const mongoString = JSON.parse(parameterPromise)
+
 // LISTEN TO APP
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   try {
-    await mongoose.connect(secret('MONGO_URI'));
+    await mongoose.connect(mongoString);
     console.log(`✅ Server is running on ${PORT} and connected to MongoDB`);
   } catch (error) {
     console.log("Db connection is failed ", error);
