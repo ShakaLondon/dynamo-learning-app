3d0ac74ee997448b40e84046d310c117a1b82cc9
Shakira Pingue <sm880@kent.ac.uk>
2024-03-17 00:41:48 +0000

    Update postbuild.sh

--- packages/backend/bin/postbuild.sh
+++ packages/backend/bin/postbuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-yum -y install jq
 rm -rf ./.amplify-hosting
 
 # mkdir -p ./.amplify-hosting/compute
@@ -13,5 +12,3 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
-
-echo MONGO_URI=$(aws ssm get-parameter --name "/amplify/dxrmrwkfi1p5r/dynamo-learning-app-backend/MONGO_URI" --with-decryption --query "Parameter.Value" --output text)
